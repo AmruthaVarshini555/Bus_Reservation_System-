b49bc12f1f366fb6e045404950ed4ea277541402
AmruthaVarshini555 <119595744+AmruthaVarshini555@users.noreply.github.com>
2023-01-16 23:09:38 +0530

    Add files via upload

---
+++ Systems_C_CPP_Linux_Nov17_Group3_Sprint2/sprint2/CUT/CODE/logs/ticket.cpp
@@ -0,0 +1,6 @@
+#include"ticket.h"
+int ticket(int num){
+        if(num==0)
+                LOG_INFO("Ticket number should not be zero");
+        return num;
+}
---
+++ Systems_C_CPP_Linux_Nov17_Group3_Sprint2/sprint2/CUT/CODE/logs/ticket.h
@@ -0,0 +1,4 @@
+#include<iostream>
+#include"logger.h"
+
+int ticket(int);
