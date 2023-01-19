19a53e72efde32112583ee0ad7af863183d7fa70
AmruthaVarshini555 <119595744+AmruthaVarshini555@users.noreply.github.com>
2023-01-19 10:42:32 +0530

    Update bus.h

--- Systems_C_CPP_Linux_Nov17_Group3_Sprint2/sprint2/CUT/CODE/inc/bus.h
+++ Systems_C_CPP_Linux_Nov17_Group3_Sprint2/sprint2/CUT/CODE/inc/bus.h
@@ -11,34 +11,87 @@ using namespace std;
 class Bus
 {
 public:   
-    //member functions
-    Bus Bus(); //default constructor
-   
-    // METHODS
-    void addBus();
-    void showAllBus();
-    void showBusDetails();
-    void viewBusDetails();
-    void deleteBus();
-    void editBus();
-    // GETTERS
-    char *getBusNo();
-    char *getSource();
-    char *getDestination();
-    char *getSourceTime();
-    char *getDestinationTime();
-    int getBookedSeats();
-    int getMaxSeats();
-    double getBusFare();
-    
-    // SETTERS
-    void setBookedSeats();
-    void setCancelTicket();
-    void setSource(char *s);
-    void setDestination(char *d);
-    void setSourceTime(char *s);
-    void setDestinationTime(char *d)
-    void setBusFare(double f);
+    Bus::Bus()
+{
+        strcpy(busNo, "");
+        maxSeats = TOTALSEATS;
+        bookedSeats = BOOKEDSEATS;
+        busFare = FARE;
+        strcpy(source, "");
+        strcpy(destination, "");
+        strcpy(sourceTime, "");
+        strcpy(destinationTime, "");
+	LOG_INFO("\nDefault constructor invoked..");
+}
+
+//Getters
+char Bus::*getBusNo()
+    {
+        return busNo;
+    }
+
+    char Bus:: *getSource()
+    {
+        return source;
+    }
+    char Bus:: *getDestination()
+    {
+        return destination;
+    }
+    char Bus::*getSourceTime()
+    {
+        return sourceTime;
+    }
+    char Bus::*getDestinationTime()
+    {
+        return destinationTime;
+    }
+    int Bus:: getBookedSeats()
+    {
+        return bookedSeats;
+    }
+    int Bus:: getMaxSeats()
+    {
+        return maxSeats;
+    }
+    double Bus:: getBusFare()
+    {
+        return busFare;
+    }
+ // SETTERS
+    void Bus::setBookedSeats()
+    {
+        bookedSeats=bookedSeats + 1;
+    }
+    void Bus::setCancelTicket()
+    {
+       bookedSeats=bookedSeats - 1;
+    }
+    void Bus:: setSource(char *s)
+    {
+        if (s && s[0])
+            strcpy(source, s);
+    }
+    void Bus:: setDestination(char *d)
+    {
+        if (d && d[0])
+            strcpy(destination, d);
+    }
+    void Bus:: setSourceTime(char *s)
+    {
+        if (s && s[0])
+            strcpy(sourceTime, s);
+    }
+   void  Bus::setDestinationTime(char *d)
+    {
+        if (d && d[0])
+            strcpy(destinationTime, d);
+    }
+    void Bus:: setBusFare(double f)
+    {
+        if (f)
+            busFare = f;
+    }
     
     ~Bus(){
         LOG_INFO("\nBus Destructor invoked");
